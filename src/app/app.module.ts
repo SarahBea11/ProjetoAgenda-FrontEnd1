e92609237446026ab0f504b449ad883bab41178b
SarahBea11 <sarahbealimapina@gmail.com>
2024-12-09 16:17:00 -0300

    Instalando bootstrap e json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgendaComponent } from './agenda/agenda.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendaComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
